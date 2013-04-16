dd7543eac320db688386cbc82464d0c6f8032f2b
Andrey Serdjuk <ase6120@gmail.com>
2013-04-16 08:04:32 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru">
+
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<title>Лучшие за сутки / Посты / Хабрахабр</title>
+	<meta name="description" content="Хабрахабр – самое крупное в Рунете сообщество людей, занятых в индустрии высоких технологий. Уникальная аудитория, свежая информация, конструктивное общение и коллективное творчество – всё это делает Хабрахабр самым оригинальным IT-проектом в России.">		
+	<link href="css/bootstrap.css" rel="stylesheet" media="screen">
+	<!-- <link href="css/bootstrap-responsive.css" rel="stylesheet" media="screen"> -->
+	<link href="css/main.css" rel="stylesheet" media="screen">
+
+	<link href="//lucky/js/lib/dijit/themes/claro/claro.css" rel="stylesheet" type="text/css" media="all">
+	<link href="//lucky/js/lib/dojox/widget/Toaster/Toaster.css" rel="stylesheet" type="text/css" media="all">
+
+	<script>
+		var dojoDirectiory = 'lib';
+		var dojoConfig = {
+				isDebug: true,
+				baseUrl: "//lucky/js/",
+				async: true, 
+				parseOnLoad: true,
+				packages: [
+					{ name: "dojo", location: dojoDirectiory + "/dojo" },
+					{ name: "dijit", location: dojoDirectiory + "/dijit" },
+					{ name: "dojox", location: dojoDirectiory + "/dojox" },
+					{ name: "engine", location: "engine" },
+				]
+			};
+	</script>
+	<script type="text/javascript" src="//lucky/js/lib/dojo/dojo.js"></script>
+
+</head>
+<body>
+	<div class="container" style="max-width:940px;">
+		<!-- userdata -->
+		<div id="head-container">
+
+			<!-- top strip -->
+			<div class="navbar navbar-fixed-top" style="position:static; margin-bottom: 10px;">
+		      <div class="navbar-inner" style="box-shadow:none; border:0; background-color:#fafafa; background-image:none; position:static;">
+		        <div class="container">
+		          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+		          </button>
+		          
+		          <div class="nav-collapse collapse">
+		            <ul class="nav">
+		              <li class="active"><a href="#">Home</a></li>
+		              <li><a href="#about">About</a></li>
+		              <li><a href="#contact">Contact</a></li>
+		            </ul>
+		          </div><!--/.nav-collapse -->
+		        </div>
+		      </div>
+		    </div>
+
+		    <div class="row">
+		    	<div class="span2" style="padding:0 0 15px 0;">
+					<img src="img/logo-new.png">
+				</div>
+			</div>
+			<!-- <h3 class="muted">Lucky-tour CSS</h3> -->
+
+			<!-- menu -->
+			<div class="navbar">
+	          <div class="navbar-inner">
+	            <div class="container">
+	              <ul class="nav">
+	                <li class="active"><a href="#">Home</a></li>
+	                <li><a href="#">Projects</a></li>
+	                <li><a href="#">Services</a></li>
+	                <li><a href="#">Downloads</a></li>
+	                <li><a href="#">About</a></li>
+	                <li><a href="#">Contact</a></li>
+	              </ul>
+	            </div>
+	          </div>
+	        </div>
+
+		</div>
+
+		<div id="main-container">
+
+		   <div class="row-fluid">
+				<!-- <ul class="breadcrumb">
+				  <li><a href="#">Home</a> <span class="divider">/</span></li>
+				  <li><a href="#">Library</a> <span class="divider">/</span></li>
+				  <li class="active">Data</li>
+				</ul> -->
+
+			<div class="sidebar-nav filters">
+				<span>
+					<select id="country" style="width: 160px;">
+					  <option>из Киева</option>
+					</select>
+				</span>
+				<select style="width: 160px;">
+				  <option>в Турцию</option>
+				  <option>Турция от 200$</option>
+				  <option>все страны</option>
+				</select>
+				<select style="width: 150px;">
+				  <option>от 5 ночей</option>
+				  <option>от 7 ночей</option>
+				</select>
+				<select style="width: 130px;">
+				  <option>все даты</option>
+				</select>
+				<select style="width: 130px;">
+				  <option>все цены</option>
+				</select>
+			</div>
+
+
+
+		   </div>
+			
+
+		   <div class="row-fluid">
+
+			   	<div class="span9 offers-group">
+			   		<div class="offer">
+				        <div class="row-fluid">
+				        	<h2>
+				        		<a href="">ОАЭ, Шарджа </a>
+				        		<a href="" class="price">от 597</a>
+				        	</h2>
+
+				        	<h3>Отель <a href="//">Belle Vue <b>
+				        		<span class="stars">3<span class="star">&#x0AB;</span>
+				        	</span></b></a></h3>
+
+
+			        	</div>
+				        <div class="row-fluid">
+				        	<div class="span4">
+								<div>
+									<img src="img/hotels/158558.jpg">
+								</div>
+							</div>
+				        	<div class="span6">
+				        		<div class="offers row-fluid">
+									<ul class="tours-list announce">
+										<li>
+											<a class="followLink" href="//">
+												<span class="date">8 мая, <em>Ср.</em></span>
+												<span class="nights">7 ночей</span>
+												<span class="meal">AI</span>
+												<span class="price">615$</span>
+												<i class="icon-shopping-cart cart-ico"></i>
+											</a>
+										</li>
+										<li>
+											<a class="followLink" href="//">
+												<span class="date">8 мая, <em>Ср.</em></span>
+												<span class="nights">10 ночей</span>
+												<span class="meal">AI</span>
+												<span class="price">681$</span>
+												<i class="icon-shopping-cart cart-ico"></i>
+											</a>
+										</li>
+										<li>
+											<a class="followLink" href="//">
+												<span class="date">8 мая, <em>Ср.</em></span>
+												<span class="nights">10 ночей</span>
+												<span class="meal">AI</span>
+												<span class="price">681$</span>
+												<i class="icon-shopping-cart cart-ico"></i>
+											</a>
+										</li>
+									</ul>
+								</div>
+				          		<div class="btn-showall"><a class="btn-small" href="#">Еще 5 туров в этот отель »</a></div>
+				        	</div>
+			        	</div>
+			   		</div>
+
+					<div class="offer">
+				        <div class="row-fluid">
+				        	<h2>
+				        		<a href="">ОАЭ, Шарджа </a>
+				        		<a href="" class="price">от 597</a>
+				        	</h2>
+
+				        	<h3>Отель <a href="//">Belle Vue <b>
+				        		<span class="stars">3<span class="star">&#x0AB;</span>
+				        	</span></b></a></h3>
+
+
+			        	</div>
+				        <div class="row-fluid">
+				        	<div class="span4">
+								<div>
+									<img src="img/hotels/158558.jpg">
+								</div>
+							</div>
+				        	<div class="span6">
+				        		<div class="offers row-fluid">
+									<ul class="tours-list announce">
+										<li>
+											<a class="followLink" href="//">
+												<span class="date">8 мая, <em>Ср.</em></span>
+												<span class="nights">7 ночей</span>
+												<span class="meal">AI</span>
+												<span class="price">615$</span>
+												<i class="icon-shopping-cart cart-ico"></i>
+											</a>
+										</li>
+										<li>
+											<a class="followLink" href="//">
+												<span class="date">8 мая, <em>Ср.</em></span>
+												<span class="nights">10 ночей</span>
+												<span class="meal">AI</span>
+												<span class="price">681$</span>
+												<i class="icon-shopping-cart cart-ico"></i>
+											</a>
+										</li>
+										<li>
+											<a class="followLink" href="//">
+												<span class="date">8 мая, <em>Ср.</em></span>
+												<span class="nights">10 ночей</span>
+												<span class="meal">AI</span>
+												<span class="price">681$</span>
+												<i class="icon-shopping-cart cart-ico"></i>
+											</a>
+										</li>
+									</ul>
+								</div>
+				          		<div class="btn-showall"><a class="btn-small" href="#">Еще 5 туров в этот отель »</a></div>
+				        	</div>
+			        	</div>
+			   		</div>
+			   	</div>
+			   	<div class="span3 sidebar-max767">
+			   		<div class="well sidebar-nav subscribe">
+
+			   			<h3>Обратный звонок</h3>
+			   			<form action="/callback" name="subcribe-callback" method="post">
+							<select class="input-country_code" type="" name="country_code" value="38">
+								<option value="38">+38</option>
+							</select>
+							<input class="input-phone" type="tel" name="phone-sms" value="" placeholder="(000) 000-00-00">
+							<span class="btn btn-small btn-info">Позвонить мне!</span>
+			   			</form>
+			   		</div>
+
+			   		<div class="well sidebar-nav subscribe">
+			   			<h3>Подписка на горящие туры</h3>
+						<form action="/subscribtion" name="subcribe-regular" method="post">
+							<label for="email-input">На e-mail:</label>
+							<input type="email" name="subscribe[email_form][target_value]" id="email-input" value="" placeholder="e-mail">
+
+							<label for="phone-sms">На телефон SMS-кой: <span>Бесплатно!</span></label>
+							<select name="subscription[phone_form][country_code]">
+								<option value="38" selected="selected">+38</option>
+							</select>
+							<input type="tel" name="subscription[phone_form][phone-sms]" id="phone-sms" value="" placeholder="(000) 000-00-00">
+							<input type="hidden" name="subscription[frequency]" value="1">
+							<input type="hidden" name="subscription[receive_promo]" value="1">
+							<input type="hidden" name="subscription[pack_form][star_id]" value="0">
+							<input type="hidden" name="subscription[pack_form][nights]" value="0">
+							<input type="hidden" name="subscription[pack_form][meal_id]" value="0">
+							<input type="hidden" name="subscription[pack_form][price_to]" value="0">
+							<input type="hidden" name="subscription[pack_form][city_id]" value="1">
+							<input type="hidden" name="subscription[pack_form][country_pack_form][country_ids]" value="0">
+						</form>
+					</div>
+			   	</div>
+
+	      </div>
+
+		</div>
+
+		<hr>
+
+		<div id="footer-container" class="footer">
+			<p>&copy; Lucky-Tour 2013</p>
+		</div>
+	</div>
+</body>
